a3e0d5c8ca7dbf4d360c048b32ce7bd3f794af90
Saurabh Jarial <bravesoul349@gmail.com>
2021-10-29 16:11:34 +0530

    walleye: Build vendor

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 $(call inherit-product, device/google/walleye/device.mk)
 $(call inherit-product-if-exists, vendor/google_devices/walleye/proprietary/device-vendor-walleye.mk)
+$(call inherit-product, vendor/google/walleye/walleye-vendor.mk)
 
 PRODUCT_PACKAGES += \
     vndk_package
